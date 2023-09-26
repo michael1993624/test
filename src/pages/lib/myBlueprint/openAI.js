c42d60be9ffb28b48eaef414f46c877b1c8733fd
unknown <michael1993624@gmail.com>
2023-09-26 16:44:00 +0900

    deleted openai

--- src/pages/lib/myBlueprint/openAI.js
+++
@@ -1,7 +0,0 @@
-var Model;
-(function (Model) {
-    Model["GPT_4"] = "gpt-4";
-    Model["GPT_3_5_turbo"] = "gpt-3.5-turbo";
-    Model["GPT_3_5_turbo_0613"] = "gpt-3.5-turbo-0613";
-    Model["GPT_3_5_turbo_16k"] = "gpt-3.5-turbo-16k";
-})(Model = exports.Model || (exports.Model = {}));
